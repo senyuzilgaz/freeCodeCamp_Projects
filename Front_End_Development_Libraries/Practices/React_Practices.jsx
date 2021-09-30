16ea2ca3486e83dd5bf17b16cb14d64331992373
Ilgaz Şenyüz <ilgazsenyuz@hotmail.com>
2021-09-30 18:04:50 +0300

    edit React

---
+++ Front_End_Development_Libraries/Practices/React_Practices.jsx
@@ -0,0 +1,9 @@
+const JSX = <div id="wrapper">
+                <h1> Hi!</h1>
+                <p> Here are 3 things that I love</p>
+                <ul>
+                  <li> Playing Chess   </li>
+                  <li> Learning New Stuff  </li>
+                  <li> Watching Films  </li>
+                </ul>
+            </div>
