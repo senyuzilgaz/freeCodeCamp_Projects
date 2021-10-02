7e9326e794991befe30209c34a7e0a9d5dfdba51
Ilgaz Şenyüz <ilgazsenyuz@hotmail.com>
2021-10-02 14:09:38 +0300

    edit react practices

--- Front_End_Development_Libraries/Practices/React_Practices.jsx
+++ Front_End_Development_Libraries/Practices/React_Practices.jsx
@@ -36,4 +36,26 @@ const MyComponent = function() {
   return ( <div> I have created component </div>);
 };
 
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+class ParentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>I am the parent</h1>
+        < ChildComponent />
+      </div>
+    );
+  }
+};
+
 ReactDOM.render(JSX, document.getElementById("challenge-node"));
