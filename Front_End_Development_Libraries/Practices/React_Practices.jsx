8a126604a6ebeb0de09789bd6251b5f62fa748ca
Ilgaz Şenyüz <ilgazsenyuz@hotmail.com>
2021-10-02 23:17:32 +0300

    edit react practices

--- Front_End_Development_Libraries/Practices/React_Practices.jsx
+++ Front_End_Development_Libraries/Practices/React_Practices.jsx
@@ -94,5 +94,34 @@ class TypesOfFood extends React.Component {
     );
   }
 };
+class Fruits extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+        <NonCitrus />
+        <Citrus />
+      </div>
+    );
+  }
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+     super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        <Fruits />
+        <Vegetables />
+      </div>
+    );
+  }
+};
 
 ReactDOM.render(JSX, document.getElementById("challenge-node"));
